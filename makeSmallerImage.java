a00c61d17372b8ecdd8456ffa7075984cb38e504
Ahmet Furkan Sevim <ahmetfurkansevim5.gmail.com>
2022-07-23 18:33:46 +0300

    Image size reducer added

---
+++ makeSmallerImage.java
@@ -0,0 +1,23 @@
+package com.empedocles.artbookjava;
+
+import android.graphics.Bitmap;
+
+public class ImageSmaller {
+    static public Bitmap makeSmallerImage(Bitmap image, int maximumSize) {
+
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        float bitmapRatio = (float) width / (float) height;
+
+        if (bitmapRatio > 1) {
+            width = maximumSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maximumSize;
+            width = (int) (height * bitmapRatio);
+        }
+
+        return Bitmap.createScaledBitmap(image,width,height,true);
+    }
+}
